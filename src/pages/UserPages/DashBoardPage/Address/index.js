6856811a667af9e0c680cde973cabb1ec9b0bf88
MinhPhucNguyen <2051063667@e.tlu.edu.vn>
2022-08-09 22:19:41 +0700

    Up code Address

--- src/pages/UserPages/DashBoardPage/Address/index.js
+++ src/pages/UserPages/DashBoardPage/Address/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import AddNewAddressModal from './AddNewAddressModal';
 import './Address.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 function Address() {
     const [openAddNewAddressModal, setOpenAddNewAddressModal] = useState(false);
@@ -19,7 +21,20 @@ function Address() {
                 </button>
             </div>
 
-            <div className="address-notification">You have no saved address!</div>
+            {/* <div className="address-notification">You have no saved address!</div> */}
+            <div className="contain_new_address">
+                <div className="address_item">
+                    <div className="address_box">
+                        <div className="info_address">
+                            <h4>Phúc Nguyễn</h4>
+                            <p>abc, abc, abc-123456</p>
+                        </div>
+                        <div className="trash_btn">
+                            <FontAwesomeIcon icon={faTrashCan} />
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             {openAddNewAddressModal && <AddNewAddressModal setOpenAddNewAddressModal={setOpenAddNewAddressModal} />}
         </div>
